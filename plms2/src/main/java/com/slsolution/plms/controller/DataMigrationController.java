81717a9349ff2b6b4ae86bec541fd21d6056228f
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-08-30 14:29:28 +0900

    회사토지 및 디비
    1. 테스트서버 작업
       - 디비 생성
       - 소스 붙이기
    2. 지상및 점용디비 원본삽입
       - 마이그레이션시 날짜가 문제가 있어 전 스크립트에서 파싱하여 전달하여 삽입하다록 처리
    3. jisa_yongdo table 명 변경 jisang_yongdo 로

--- plms2/src/main/java/com/slsolution/plms/controller/DataMigrationController.java
+++ plms2/src/main/java/com/slsolution/plms/controller/DataMigrationController.java
@@ -175,6 +175,7 @@ public class DataMigrationController {
           	else if (mapperName.equals("notset_master")) mainService.InsertQuery("migrationSQL.notsetMaster", param);
           	else if (mapperName.equals("gover_pnu")) mainService.InsertQuery("migrationSQL.goverPnu", param);
           	else if (mapperName.equals("gover_permit")) mainService.InsertQuery("migrationSQL.gover_permit", param);
+          	else if (mapperName.equals("gover_permit2")) mainService.InsertQuery("migrationSQL.gover_permit2", param);
           	else if (mapperName.equals("sigun")) mainService.InsertQuery("migrationSQL.sigun", param);
           	else if (mapperName.equals("bdong")) mainService.InsertQuery("migrationSQL.bdong", param);
           	else if (mapperName.equals("dosi_master")) mainService.InsertQuery("migrationSQL.dosi_master", param);
