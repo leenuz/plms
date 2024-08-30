31c621354ebaee7eded7a57d3e24fc8342e640ee
jmson-slsolution <jmson@slsolution.co.kr>
2024-08-30 15:35:41 +0900

    점용 마스터 총 검색 건 수 css 수정

--- plms2/src/main/resources/static/js/module/surfaceMain.js
+++ plms2/src/main/resources/static/js/module/surfaceMain.js
@@ -65,11 +65,16 @@ window.addEventListener("load" , () => {
                	return surfaceInquireSelectEvet("#dopcosurfaceInquire05 .surfaceInquireSelectsTitleBtn" ,"#dopcosurfaceInquire05 .sufaceSelectList"  ,"#dopcosurfaceInquire05 .sufaceSelectList .surfaceSelectListMember" , ".surfaceInquireHiddenSelect05");
                 handleCheckboxes(dopcosurfaceInquire05);
             }
-
 			else if(pathName === `/gover/menu03_1`){
-			    return privateUseSelectEvet("#dopcoPrivateUse01 .privateUseSelectsTitleBtn" ,"#dopcoPrivateUse01 .sufaceSelectList"  ,"#dopcoPrivateUse01 .sufaceSelectList .surfaceSelectListMember" , ".privateUseHiddenSelect01");
+			    return privateUseSelectEvet("#dopcoPrivateUse01 .privateUseSelectsTitleBtn" ,"#dopcoPrivateUse01 .sufaceSelectList"  ,"#dopcoPrivateUse01 .sufaceSelectList .surfaceSelectListMember" , ".privateUseHiddenSelect01", "inquireCheckCont01"),
+				surfaceInquireCheckboxes(inquireCheckCont01);
 			 }
-
+			else if(pathName === `/gover/masterEdit`){
+ 			   return privateUseSelectEvet("#masterEditWrappers .privateUseSelectsTitleBtn" ,"#masterEditWrappers .sufaceSelectList"  ,"#masterEditWrappers .sufaceSelectList .surfaceSelectListMember" , ".privateUseHiddenSelect01");
+ 			}
+			else if(pathName === `/gover/masterReg`){
+			   return privateUseSelectEvet("#masterRegWrappers .privateUseSelectsTitleBtn" ,"#masterRegWrappers .sufaceSelectList"  ,"#masterRegWrappers .sufaceSelectList .surfaceSelectListMember" , ".privateUseHiddenSelect01");
+			}
 			else if(pathName === `/togi/menu04_1`){
 			    return landDevelopmentManageSelectEvet("#dopcoLandDevelopmentManage .landDevelopmentManageSelectsTitleBtn" ,"#dopcoLandDevelopmentManage .sufaceSelectList"  ,"#dopcoLandDevelopmentManage .sufaceSelectList .surfaceSelectListMember" , ".landDevelopmentManageHiddenSelect");
 			}
