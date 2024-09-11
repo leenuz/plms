bee693939324e5ccf5ed9252dc71aa65dfdfb00d
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-09-11 12:43:45 +0900

    점용등록 파일 첨부
    점용파일 첨부

--- plms2/src/main/resources/static/js/gover/masterReg.js
+++ plms2/src/main/resources/static/js/gover/masterReg.js
@@ -19,134 +19,174 @@ $(document).ready(function() {
 
 
 	var objDragAndDrop = $(".fileUploadBox");
-	console.log("--------objDragAndDrop------------------------");
-	console.log(objDragAndDrop.parent().parent().html());
-	               
-   $(document).on("dragenter",".fileUploadBox",function(e){
-       e.stopPropagation();
-       e.preventDefault();
-       $(this).css('border', '2px solid #0B85A1');
-   });
-   $(document).on("dragover",".fileUploadBox",function(e){
-       e.stopPropagation();
-       e.preventDefault();
-   });
-   $(document).on("drop",".fileUploadBox",function(e){
-       
-       $(this).css('border', '2px dotted #0B85A1');
-       e.preventDefault();
-       var files = e.originalEvent.dataTransfer.files;
-   console.log(objDragAndDrop);
-       handleFileUpload(files,objDragAndDrop);
-   });
-   
-   $(document).on('dragenter', function (e){
-       e.stopPropagation();
-       e.preventDefault();
-   });
-   $(document).on('dragover', function (e){
-     e.stopPropagation();
-     e.preventDefault();
-     objDragAndDrop.css('border', '2px dotted #0B85A1');
-   });
-   $(document).on('drop', function (e){
-       e.stopPropagation();
-       e.preventDefault();
-   });
-   //drag 영역 클릭시 파일 선택창
-   objDragAndDrop.on('click',function (e){
-       $('input[type=file]').trigger('click');
-   });
-
-   $('input[type=file]').on('change', function(e) {
-       var files = e.originalEvent.target.files;
-       handleFileUpload(files,objDragAndDrop);
-   });
-   
-   function handleFileUpload(files,obj)
-   {
-      for (var i = 0; i < files.length; i++) 
-      {
-           var fd = new FormData();
-           fd.append('file', files[i]);
-    		console.log("-----------handleFileUpload------------");
-			console.log(obj.parent().parent().find("#fileListDiv").html());
-         //  var status = new createStatusbar(obj.parent().parent().find("#fileListDiv ")); //Using this we can set progress.
-		   var status = new createStatusbar($("#fileTitleUl"),files[i].name,files[i].size,i); //Using this we can set progress.
-           //status.setFileNameSize(files[i].name,files[i].size);
-           sendFileToServer(fd,status);
-    
-      }
-   }
-   
-   var rowCount=0;
-   function createStatusbar(obj,name,size,no){
-          console.log("----------start createStatusBar------------");
-              console.log(obj.html());
-
-          var sizeStr="";
-                                  var sizeKB = size/1024;
-                                  if(parseInt(sizeKB) > 1024){
-                                      var sizeMB = sizeKB/1024;
-                                      sizeStr = sizeMB.toFixed(2)+" MB";
-                                  }else{
-                                      sizeStr = sizeKB.toFixed(2)+" KB";
-                                  }
-
-          var row='<ul class="contents" id="fileListUl">';
-          row+='<li class="selectWidth content checkboxWrap">';
-          row+='<input type="checkbox" id="landRightsRegistration_attachFile'+no+'" name="landRightsRegistration_attachFile" >';
-          row+='<label for="landRightsRegistration_attachFile'+no+'"></label>';
-          row+='</li>';
-          row+='<li class="content registDateWidth"><input type="text" id="filename" th:placeholder="'+'[[${val.pa_file_path}]]'+'" class="notWriteInput" readonly></li>';
-          row+='<li class="content fileNameWidth"><input type="text" id="filename" placeholder="'+name+'" class="notWriteInput" readonly></li>';
-          row+='<li class="content"><button class="viewDetailButton" th:onclick="openFilePopup([[${val.pa_file_path}]])">보기</button></li></ul>';
-          obj.after(row);
-
-          var radio=$(row).find('input');
-          console.log("---------------radio checkbox----------");
-          $(radio).find('input').attr("disabled",false);
-          console.log($(radio).parent().html());
-      }
-   
-   function sendFileToServer(formData,status)
-   {
-       var uploadURL = "/gover/fileUpload/post"; //Upload URL
-       var extraData ={}; //Extra Data.
-       var jqXHR=$.ajax({
-               xhr: function() {
-               var xhrobj = $.ajaxSettings.xhr();
-               if (xhrobj.upload) {
-                       xhrobj.upload.addEventListener('progress', function(event) {
-                           var percent = 0;
-                           var position = event.loaded || event.position;
-                           var total = event.total;
-                           if (event.lengthComputable) {
-                               percent = Math.ceil(position / total * 100);
-                           }
-                           //Set progress
-                           status.setProgress(percent);
-                       }, false);
-                   }
-               return xhrobj;
-           },
-           url: uploadURL,
-           type: "POST",
-           contentType:false,
-           processData: false,
-           cache: false,
-           data: formData,
-           success: function(data){
-               status.setProgress(100);
-    			console.log(data);
-    			console.log(data.resultData);
-               //$("#status1").append("File upload Done<br>");    
-			uploadFiles.push(data.resultData.fpath);       
-           }
-       }); 
-    
-       status.setAbort(jqXHR);
-   }
+	                
+	                $(document).on("dragenter",".fileUploadBox",function(e){
+	                    e.stopPropagation();
+	                    e.preventDefault();
+	                    $(this).css('border', '2px solid #0B85A1');
+	                });
+	                $(document).on("dragover",".fileUploadBox",function(e){
+	                    e.stopPropagation();
+	                    e.preventDefault();
+	                });
+	                $(document).on("drop",".fileUploadBox",function(e){
+	                    
+	                    $(this).css('border', '2px dotted #0B85A1');
+	                    e.preventDefault();
+	                    var files = e.originalEvent.dataTransfer.files;
+	                
+	                    handleFileUpload(files,objDragAndDrop);
+	                });
+	                
+	                $(document).on('dragenter', function (e){
+	                    e.stopPropagation();
+	                    e.preventDefault();
+	                });
+	                $(document).on('dragover', function (e){
+	                  e.stopPropagation();
+	                  e.preventDefault();
+	                  objDragAndDrop.css('border', '2px dotted #0B85A1');
+	                });
+	                $(document).on('drop', function (e){
+	                    e.stopPropagation();
+	                    e.preventDefault();
+	                });
+	                //drag 영역 클릭시 파일 선택창
+	                objDragAndDrop.on('click',function (e){
+	                    $('input[type=file]').trigger('click');
+	                });
+	 
+	                $('input[type=file]').on('change', function(e) {
+	                    var files = e.originalEvent.target.files;
+	                    handleFileUpload(files,objDragAndDrop);
+	                });
+	                
+	                function handleFileUpload(files,obj)
+	                {
+						console.log("-------------handleFileUpload---------------");
+						console.log(files);
+	                   for (var i = 0; i < files.length; i++) 
+	                   {
+	                        var fd = new FormData();
+	                        fd.append('file', files[i]);
+	                 		
+	                        var status = new createStatusbar($("#fileTitleUl"),files[i].name,files[i].size,i); //Using this we can set progress.
+	                      //  status.setFileNameSize(files[i].name,files[i].size);
+	                        sendFileToServer(fd,status);
+	                 
+	                   }
+	                }
+	                
+	                var rowCount=0;
+	                function createStatusbar(obj,name,size,no){
+						console.log("----------start createStatusBar------------");
+	                        console.log(obj.html());
+						/*var uobj=obj.parent().parent().find("#status");	
+	                    rowCount++;
+	                    var row="";
+	                    //if(rowCount %2 ==0) row ="even";
+	                    this.statusbar = $('<ul class="contents" id="fileListUl">');
+	                    this.filename = $('<div class='filename'></div>').appendTo(this.statusbar);
+	                    this.size = $("<div class='filesize'></div>").appendTo(this.statusbar);
+	                   // this.progressBar = $("<div class='progressBar'><div></div></div>").appendTo(this.statusbar);
+	                    this.abort = $("<div class='abort'>중지</div>").appendTo(this.statusbar);*/
+						
+						var sizeStr="";
+						                        var sizeKB = size/1024;
+						                        if(parseInt(sizeKB) > 1024){
+						                            var sizeMB = sizeKB/1024;
+						                            sizeStr = sizeMB.toFixed(2)+" MB";
+						                        }else{
+						                            sizeStr = sizeKB.toFixed(2)+" KB";
+						                        }
+						
+	                    var row='<ul class="contents" id="fileListUl">';
+						row+='<li class="content01 content checkboxWrap">';
+						row+='<input type="checkbox" id="landRightsRegistration_attachFile'+no+'" name="landRightsRegistration_attachFile" >';
+						row+='<label for="landRightsRegistration_attachFile'+no+'"></label>';
+						row+='</li>';
+						row+='<li class="content02 content"><input type="text" id="filename" placeholder="'+name+'" class="notWriteInput" readonly></li></ul>';
+	                    obj.after(row);
+						
+						var radio=$(row).find('input');
+						console.log("---------------radio checkbox----------");
+						$(radio).find('input').attr("disabled",false);
+	                 	console.log($(radio).parent().html());
+						
+	                   /* this.setFileNameSize = function(name,size){
+	                        var sizeStr="";
+	                        var sizeKB = size/1024;
+	                        if(parseInt(sizeKB) > 1024){
+	                            var sizeMB = sizeKB/1024;
+	                            sizeStr = sizeMB.toFixed(2)+" MB";
+	                        }else{
+	                            sizeStr = sizeKB.toFixed(2)+" KB";
+	                        }
+	                 
+	                        $(#)
+	                        this.size.html(sizeStr);
+	                    }*/
+	                    
+	                    /*this.setProgress = function(progress){       
+	                        var progressBarWidth =progress*this.progressBar.width()/ 100;  
+	                        this.progressBar.find('div').animate({ width: progressBarWidth }, 10).html(progress + "% ");
+	                        if(parseInt(progress) >= 100)
+	                        {
+	                            this.abort.hide();
+	                        }
+	                    }
+	                    
+	                    this.setAbort = function(jqxhr){
+	                        var sb = this.statusbar;
+	                        this.abort.click(function()
+	                        {
+	                            jqxhr.abort();
+	                            sb.hide();
+	                        });
+	                    }*/
+	                }
+	                
+	                function sendFileToServer(formData,status)
+	                {
+	                    var uploadURL = "/jisang/fileUpload/post"; //Upload URL
+	                    var extraData ={}; //Extra Data.
+	                    var jqXHR=$.ajax({
+	                            xhr: function() {
+	                            var xhrobj = $.ajaxSettings.xhr();
+	                            if (xhrobj.upload) {
+	                                    xhrobj.upload.addEventListener('progress', function(event) {
+	                                        var percent = 0;
+	                                        var position = event.loaded || event.position;
+	                                        var total = event.total;
+	                                        if (event.lengthComputable) {
+	                                            percent = Math.ceil(position / total * 100);
+	                                        }
+	                                        //Set progress
+	                                      //  status.setProgress(percent);
+	                                    }, false);
+	                                }
+	                            return xhrobj;
+	                        },
+	                        url: uploadURL,
+	                        type: "POST",
+	                        contentType:false,
+	                        processData: false,
+	                        cache: false,
+	                        data: formData,
+	                        success: function(data){
+	                           // status.setProgress(100);
+	                 			console.log(data);
+	                 			console.log(data.resultData);
+								//console.log("-------------sendFileToServer-----------------------");
+								//console.log($(this).parent().parent().parent().parent());
+	                            //$("#status1").append("File upload Done<br>");    
+								//uploadFiles.push(data.resultData.fpath);    
+								//allCheckEventLandRightsRegist();   
+	                        }
+	                    }); 
+	                 
+	                    //status.setAbort(jqXHR);
+	                }
 });
 
 // 체크박스 클릭 시 다른 체크박스들 비활성화
