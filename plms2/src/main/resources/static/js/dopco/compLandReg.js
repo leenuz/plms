2a739baa0c96decb8fd74f6cc90bd9b0c528627d
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-09-21 17:56:51 +0900

    회사토지등록 UI UX
    폼 내 인자들 네이밍 변경

--- plms2/src/main/resources/static/js/dopco/compLandReg.js
+++ plms2/src/main/resources/static/js/dopco/compLandReg.js
@@ -538,7 +538,7 @@ compLandRegSearchOpenPopUp();
 
 $(document).on("click",".registBtn",function(){
 
-	var formSerializeArray = $('#searchForm').serializeArray();
+	var formSerializeArray = $('#saveForm').serializeArray();
 
        len = formSerializeArray.length;
        var dataObj = {};
@@ -594,6 +594,44 @@ $(document).on("click",".registBtn",function(){
        /* var json = JSON.stringify(formSerializeArray);
            console.log("----------jsonobj------------");
            console.log(json); // JSON 문자열 출력*/
+           return;
+       url="/dopco/insertDopcoList"; 
+	   $.ajax({
+	   			
+			url:url,
+			type:'POST',
+			contentType:"application/json",
+			data:JSON.stringify(object),
+			
+			dataType:"json",
+			beforeSend:function(request){
+				console.log("beforesend ........................");
+				loadingShow();
+			},
+			success:function(response){
+				loadingHide();
+				console.log(response);
+				if (response.success="Y"){
+					console.log("response.success Y");
+					console.log("response.resultData length:"+response.resultData.length);
+					alert("정상적으로 등록 되었습니다.");
+					/*$("#popup_bg").show();
+					$("#popup").show(500);
+					//$("#addrPopupLayer tbody td").remove();
+					for(var i=0;i<response.resultData.length;i++){
+						$("#addrPopupTable tbody").append("<tr><td>"+response.resultData[i].juso+"</td><td><button>선택</button></td></tr>");
+					}*/
+				}
+				else {
+					console.log("response.success N");
+				}
+			},
+			error:function(jqXHR,textStatus,errorThrown){
+				alert("registBtn ajax error\n"+textStatus+":"+errorThrown);
+				return false;
+			}
+		
+	}); 
 
 
 });
