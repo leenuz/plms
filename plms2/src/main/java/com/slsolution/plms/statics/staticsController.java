1f0da2a1b25c6238b62022c7ccb431053151ebbd
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-09-22 19:51:40 +0900

    권리확보마감처리 컨트롤러 생성
    생성

--- plms2/src/main/java/com/slsolution/plms/statics/staticsController.java
+++ plms2/src/main/java/com/slsolution/plms/statics/staticsController.java
@@ -29,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @EnableAsync
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/togi")
+@RequestMapping("/statics")
 @CrossOrigin(origins="*",allowedHeaders="*")
 public class staticsController {
 	@Autowired
@@ -79,113 +79,117 @@ public class staticsController {
 			rst.put("GUKYUJI_TOTAL", String.valueOf(Long.valueOf((String) rst.get("GUKYUJI_Y")) + Long.valueOf((String) rst.get("GUKYUJI_N")) + Long.valueOf((String) rst.get("GUKYUJI_J"))));
 
 			// 엑셀파일 제작
-//			HashMap params = new HashMap();
-//			params.put("JISANG_STATUS", "'GOVER','JISANG','NOTSET'");
-//			ArrayList<HashMap<String, Object>> list = (ArrayList<HashMap<String, Object>>) mainService.selectHashmapQuery("songyuSQL.selectSonguList", params);
+			HashMap params = new HashMap();
+			params.put("JISANG_STATUS", "'GOVER','JISANG','NOTSET'");
+			HashMap<String,Object> ma=mainService.selectHashmapQuery("songyuSQL.selectSonguList", params);
+			ArrayList<HashMap<String, Object>> list = new ArrayList();
+			list.add(ma);
+			//ArrayList<HashMap<String, Object>> list =(ArrayList) aaaa;
+			//ArrayList<HashMap<String, Object>> list = (ArrayList<HashMap<String, Object>>) mainService.selectHashmapQuery("songyuSQL.selectSonguList", params);
 //
 //			// 일부 문자열 처리
-//			for (HashMap excelMap : list) {
-//				// 관경
-//				if (excelMap.get("PIPE_METER2") != null && !"".equals(excelMap.get("PIPE_METER2"))) {
-//					excelMap.put("PIPE_METER_XLS", String.valueOf(excelMap.get("PIPE_METER")) + ", " + String.valueOf(excelMap.get("PIPE_METER2")));
-//				} else {
-//					excelMap.put("PIPE_METER_XLS", CommonUtil.nvl("" + excelMap.get("PIPE_METER")));
-//				}
-//				// 소유자
-//				if (excelMap.get("SOUJA_CNT") != null && Integer.parseInt("" + excelMap.get("SOUJA_CNT")) > 0) {
-//					excelMap.put("SOUJA_NAME_XLS", String.valueOf(excelMap.get("SOUJA_NAME")) + " 외" + excelMap.get("SOUJA_CNT") + "명");
-//				} else {
-//					excelMap.put("SOUJA_NAME_XLS", CommonUtil.evl("" + excelMap.get("SOUJA_NAME"), ""));
-//				}
-//				// 토지유형 문자열
-//				if (excelMap.get("GOVER_OWN_YN") != null && "Y".equals(excelMap.get("GOVER_OWN_YN"))) {
-//					excelMap.put("GOVER_OWN_YN_XLS", "국유지");
-//				} else {
-//					excelMap.put("GOVER_OWN_YN_XLS", "사유지");
-//				}
-//				// 권리확보
-//				if (excelMap.get("MASTER_NO") != null) {
-//					if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("J")) {
-//						excelMap.put("MASTER_TYPE", "지상권");
-//					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("G")) {
-//						excelMap.put("MASTER_TYPE", "점용");
-//					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("N")) {
-//						excelMap.put("MASTER_TYPE", "미설정");
-//					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("L")) {
-//						excelMap.put("MASTER_TYPE", "매입");
-//					} else {
-//						excelMap.put("MASTER_TYPE", "미설정");
-//					}
-//				}
-//				// 점용기간
-//				if (excelMap.get("PMT_ST_DATE") != null && !"".equals(excelMap.get("PMT_ST_DATE")) && excelMap.get("PMT_ED_DATE") != null && !"".equals(excelMap.get("PMT_ED_DATE"))) {
-//					excelMap.put("GOVER_PERIOD", CommonUtil.nvl((String) excelMap.get("PMT_ST_DATE")) + " ~ " + CommonUtil.nvl((String) excelMap.get("PMT_ED_DATE")));
-//				} else {
-//					excelMap.put("GOVER_PERIOD", "-");
-//				}
-//			}
-//
-//			String[] mapNames = { "RN", // 순번
-//					"JISA", // 담당지사
-//					"PIPE_OVERLAP_YN", // 관로저촉
-//					"PIPE_METER_XLS", // 관경
-//					"SIDO_NM", // 시도
-//					"SGG_NM", // 시군구
-//					"EMD_NM", // 읍면동
-//					"RI_NM", // 리
-//					"JIBUN", // 지번
-//					"JIMOK_TEXT", // 지목
-//					"SOUJA_NAME_XLS", // 소유자
-//					"JIJUK_AREA", // 면적
-//					"GOVER_OWN_YN_XLS", // 토지유형
-//					"MASTER_TYPE", // 권리확보
-//					"JASAN_NO", // 자산번호
-//					"MASTER_NO", // 관리번호
-//					"CHUIDEUK_DATE", // 취득일
-//					"GOVER_PERIOD", // 점용기간
-//					"PAY_DATE", // 점용납부일
-//					"GOVER_AREA", // 설정면적
-//					"GOVER_LENGTH", // 설정연장
-//					"JASAN_MONEY", // 자산금액
-//					"PAY_MONEY" // 납부금액
-//			};
-//			ArrayList<String> headerStr = new ArrayList<String>();
-//			headerStr.add("순번");
-//			headerStr.add("담당지사");
-//			headerStr.add("관로저촉");
-//			headerStr.add("관경");
-//			headerStr.add("시도");
-//			headerStr.add("시군구");
-//			headerStr.add("읍면동");
-//			headerStr.add("리");
-//			headerStr.add("지번");
-//			headerStr.add("지목");
-//			headerStr.add("소유자");
-//			headerStr.add("면적");
-//			headerStr.add("토지유형");
-//			headerStr.add("권리확보");
-//			headerStr.add("자산번호");
-//			headerStr.add("관리번호");
-//			headerStr.add("취득일");
-//			headerStr.add("점용기간");
-//			headerStr.add("점용납부일");
-//			headerStr.add("설정면적");
-//			headerStr.add("설정연장");
-//			headerStr.add("자산금액");
-//			headerStr.add("납부금액");
-//
-//			String excelFileName = ExcelUtil.makeMapList2Excel(excelFIleBase + "/uploadfile/statExcel", "권리확보현황", headerStr, mapNames, list);
-//
-//			rst.put("FILE_PATH", "/uploadfile/statExcel/" + excelFileName);
-//			rst.put("FILE_NAME", excelFileName);
-//			System.out.println("권리확보현황 마감처리 ::: " + rst.toString());
-//			int result = Database.getInstance().update("Json.insertStatisticsDeadlineProcess", rst);
-//
-//			if (result > 0) {
-//				map.put("result", "success");
-//			} else {
-//				map.put("result", "ERROR");
-//			}
+			for (HashMap excelMap : list) {
+				// 관경
+				if (excelMap.get("PIPE_METER2") != null && !"".equals(excelMap.get("PIPE_METER2"))) {
+					excelMap.put("PIPE_METER_XLS", String.valueOf(excelMap.get("PIPE_METER")) + ", " + String.valueOf(excelMap.get("PIPE_METER2")));
+				} else {
+					excelMap.put("PIPE_METER_XLS", CommonUtil.nvl("" + excelMap.get("PIPE_METER")));
+				}
+				// 소유자
+				if (excelMap.get("SOUJA_CNT") != null && Integer.parseInt("" + excelMap.get("SOUJA_CNT")) > 0) {
+					excelMap.put("SOUJA_NAME_XLS", String.valueOf(excelMap.get("SOUJA_NAME")) + " 외" + excelMap.get("SOUJA_CNT") + "명");
+				} else {
+					excelMap.put("SOUJA_NAME_XLS", CommonUtil.evl("" + excelMap.get("SOUJA_NAME"), ""));
+				}
+				// 토지유형 문자열
+				if (excelMap.get("GOVER_OWN_YN") != null && "Y".equals(excelMap.get("GOVER_OWN_YN"))) {
+					excelMap.put("GOVER_OWN_YN_XLS", "국유지");
+				} else {
+					excelMap.put("GOVER_OWN_YN_XLS", "사유지");
+				}
+				// 권리확보
+				if (excelMap.get("MASTER_NO") != null) {
+					if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("J")) {
+						excelMap.put("MASTER_TYPE", "지상권");
+					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("G")) {
+						excelMap.put("MASTER_TYPE", "점용");
+					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("N")) {
+						excelMap.put("MASTER_TYPE", "미설정");
+					} else if (String.valueOf(excelMap.get("MASTER_NO")).startsWith("L")) {
+						excelMap.put("MASTER_TYPE", "매입");
+					} else {
+						excelMap.put("MASTER_TYPE", "미설정");
+					}
+				}
+				// 점용기간
+				if (excelMap.get("PMT_ST_DATE") != null && !"".equals(excelMap.get("PMT_ST_DATE")) && excelMap.get("PMT_ED_DATE") != null && !"".equals(excelMap.get("PMT_ED_DATE"))) {
+					excelMap.put("GOVER_PERIOD", CommonUtil.nvl((String) excelMap.get("PMT_ST_DATE")) + " ~ " + CommonUtil.nvl((String) excelMap.get("PMT_ED_DATE")));
+				} else {
+					excelMap.put("GOVER_PERIOD", "-");
+				}
+			}
+
+			String[] mapNames = { "RN", // 순번
+					"JISA", // 담당지사
+					"PIPE_OVERLAP_YN", // 관로저촉
+					"PIPE_METER_XLS", // 관경
+					"SIDO_NM", // 시도
+					"SGG_NM", // 시군구
+					"EMD_NM", // 읍면동
+					"RI_NM", // 리
+					"JIBUN", // 지번
+					"JIMOK_TEXT", // 지목
+					"SOUJA_NAME_XLS", // 소유자
+					"JIJUK_AREA", // 면적
+					"GOVER_OWN_YN_XLS", // 토지유형
+					"MASTER_TYPE", // 권리확보
+					"JASAN_NO", // 자산번호
+					"MASTER_NO", // 관리번호
+					"CHUIDEUK_DATE", // 취득일
+					"GOVER_PERIOD", // 점용기간
+					"PAY_DATE", // 점용납부일
+					"GOVER_AREA", // 설정면적
+					"GOVER_LENGTH", // 설정연장
+					"JASAN_MONEY", // 자산금액
+					"PAY_MONEY" // 납부금액
+			};
+			ArrayList<String> headerStr = new ArrayList<String>();
+			headerStr.add("순번");
+			headerStr.add("담당지사");
+			headerStr.add("관로저촉");
+			headerStr.add("관경");
+			headerStr.add("시도");
+			headerStr.add("시군구");
+			headerStr.add("읍면동");
+			headerStr.add("리");
+			headerStr.add("지번");
+			headerStr.add("지목");
+			headerStr.add("소유자");
+			headerStr.add("면적");
+			headerStr.add("토지유형");
+			headerStr.add("권리확보");
+			headerStr.add("자산번호");
+			headerStr.add("관리번호");
+			headerStr.add("취득일");
+			headerStr.add("점용기간");
+			headerStr.add("점용납부일");
+			headerStr.add("설정면적");
+			headerStr.add("설정연장");
+			headerStr.add("자산금액");
+			headerStr.add("납부금액");
+
+			String excelFileName = ExcelUtil.makeMapList2Excel(excelFIleBase + "/uploadfile/statExcel", "권리확보현황", headerStr, mapNames, list);
+
+			rst.put("FILE_PATH", "/uploadfile/statExcel/" + excelFileName);
+			rst.put("FILE_NAME", excelFileName);
+			System.out.println("권리확보현황 마감처리 ::: " + rst.toString());
+			int result =(int) mainService.InsertQuery("staticSQL.insertStatisticsDeadlineProcess", rst);
+
+			if (result > 0) {
+				map.put("result", "success");
+			} else {
+				map.put("result", "ERROR");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			map.put("result", "ERROR");
@@ -241,8 +245,8 @@ public class staticsController {
 				// System.out.println(params);
 
 				sayujiList = (ArrayList) mainService.selectQuery("issueSQL.selectTogiMgtJisangSearch", params);
-				//gukyujiList = (ArrayList) Database.getInstance().queryForList("Json.selectTogiMgtGoverSearch", params);
-//				issueList = (ArrayList) Database.getInstance().queryForList("Json.searchStatisticsTojiCount", params);
+				gukyujiList = (ArrayList) mainService.selectQuery("goverSQL.selectTogiMgtGoverSearch", params);
+				issueList = (ArrayList) mainService.selectQuery("staticSQL.searchStatisticsTojiCount", params);
 
 				if (sayujiList.size() > 0) {
 					for (int i = 0; i < sayujiList.size(); i++) {
