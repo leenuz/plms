4c52f3cd59c797c2ac18b0ce99082d3f8c75cf22
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-09-29 15:45:31 +0900

    T0029 완료
    완료

--- plms2/src/main/resources/static/js/gover/occupationDetails.js
+++ plms2/src/main/resources/static/js/gover/occupationDetails.js
@@ -47,6 +47,11 @@ if(registBtn) {
     })
 }
 
+
+
+
+
+
 /* 잠재이슈 수정 팝업 오픈 */
 
 const occupationIssueReviseOpen = () => {
@@ -380,6 +385,30 @@ $(document).on("click",".delBtn",function(){
 
 	  });
 });
+
+
+
+//보기 클릭 시 팝업 기능
+function openFilePopup(filePath) {
+	console.log(filePath);
+    // 절대 경로를 사용하도록 file:// 스킴을 추가
+    const serverUrl = `/api/downloadFile?filePath=` + encodeURIComponent(filePath);
+
+    // 새 창의 옵션 설정 (예: 너비 600px, 높이 400px, 스크롤바 허용 등)
+    const popupOptions = "width=800,height=600,scrollbars=yes,resizable=yes";
+    // 새 창 열기
+    window.open(serverUrl, '파일 보기', popupOptions);
+}
+
+//다운로드 스크립트
+function downloadFile(filePath, fileName) {
+      const url = `/api/download?filePath=${filePath}&fileName=${encodeURIComponent(fileName)}`;
+	 //  const url = `/api/download?filePath=${filePath}&fileName=${fileName}`;
+	   console.log(url);
+       window.open(url, '_blank');  // 새 창이나 새 탭에서 파일 다운로드
+   }
+
+
 
 $(function() {
     $('#jijukNewWindowBtn').click(function() {
