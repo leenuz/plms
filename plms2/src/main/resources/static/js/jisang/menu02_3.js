62c9fe77e6fd45c4dd37c928abc21352e8be333d
jmson-slsolution <jmson@slsolution.co.kr>
2024-10-06 16:21:05 +0900

    지상권분할(menu03_3)-주소검색-선택형 수정+데이터 테이블에 emd 도 나오도록 수정

--- plms2/src/main/resources/static/js/jisang/menu02_3.js
+++ plms2/src/main/resources/static/js/jisang/menu02_3.js
@@ -1,234 +1,234 @@
-    
+
 var table;
 
 $(document).ready(function() {
-  console.log("jisang/menu02_3.js start");
-  /*$('#jisa').niceSelect();*/
-//testAjax();
-//init_Table();
-loadDataTable("");
+	console.log("jisang/menu02_3.js start");
+	/*$('#jisa').niceSelect();*/
+	//testAjax();
+	//init_Table();
+	loadDataTable("");
 
 });
 
-$(document).on("click",".finalBtn",function(){
+$(document).on("click", ".finalBtn", function() {
 
 	console.log($("#searchResultPopDiv").html());
-	var targetDiv=$("#searchResultPopDiv").parent().find("#searchResultPopup").find(".popupWrap");
+	var targetDiv = $("#searchResultPopDiv").parent().find("#searchResultPopup").find(".popupWrap");
 	//console.log($(targetDiv).html());
 	$("#popupWrap").toggleClass("active");
 });
 
-$(document).on("click",".topCloseBtn",function(){
+$(document).on("click", ".topCloseBtn", function() {
 
 	console.log($("#searchResultPopDiv").html());
-	var targetDiv=$("#searchResultPopDiv").parent().find("#searchResultPopup").find(".popupWrap");
+	var targetDiv = $("#searchResultPopDiv").parent().find("#searchResultPopup").find(".popupWrap");
 	//console.log($(targetDiv).html());
 	$("#popupWrap").toggleClass("active");
 });
 
 //조회하기 클릭시 상단 정보 출력 (현재는 지사 부분만 추가하였음 ... 다 불수 있게 추가해주세요)
-$(document).on("click","#registerBtn",function(){
-       console.log($("#menuHiddenSelectBox01_1").val());
-	   console.log($("#searchForm").serialize());
+$(document).on("click", "#registerBtn", function() {
+	console.log($("#menuHiddenSelectBox01_1").val());
+	console.log($("#searchForm").serialize());
 
-	   var formSerializeArray = $('#searchForm').serializeArray();
-	   console.log(formSerializeArray)
-	   var object = {};
-	   for (var i = 0; i < formSerializeArray.length; i++){
-	       object[formSerializeArray[i]['name']] = formSerializeArray[i]['value'];
-	   }
+	var formSerializeArray = $('#searchForm').serializeArray();
+	console.log(formSerializeArray)
+	var object = {};
+	for (var i = 0; i < formSerializeArray.length; i++) {
+		object[formSerializeArray[i]['name']] = formSerializeArray[i]['value'];
+	}
 
-	   var json = JSON.stringify(formSerializeArray);
+	var json = JSON.stringify(formSerializeArray);
 
-	  console.log("----------jsonobj------------");
-	  console.log(json);
-	  console.log("object askMenu01:"+object.askMenu01);
+	console.log("----------jsonobj------------");
+	console.log(json);
+	console.log("object askMenu01:" + object.askMenu01);
 
 
 
-	   loadDataTable(object);
-	   console.log("-----------------------");
+	loadDataTable(object);
+	console.log("-----------------------");
 
-     })
+})
 
-$(document).on("change","#sido",function(){
+$(document).on("change", "#sido", function() {
 	console.log("----------start sido change -------------");
-	$("#sido").val($("#sidoText").text()).attr("selected","selected");
-	if ($("#sido").val()==null) return;
-	var allData={"key":$("#sido").val()}
-					   console.log(allData);
-					  $.ajax({
+	$("#sido").val($("#sidoText").text()).attr("selected", "selected");
+	if ($("#sido").val() == null) return;
+	var allData = { "key": $("#sido").val() }
+	console.log(allData);
+	$.ajax({
 
-					    url: "/land/api/getSigunMaster",
-					    data:JSON.stringify(allData),
-					    async: true,
-					    type:"POST",
-					    dataType: "json",
-					    contentType: 'application/json; charset=utf-8',
-					    success: function(rt,jqXHR) {
-					      console.log(rt);
-						  var data=rt.resultData;
+		url: "/land/api/getSigunMaster",
+		data: JSON.stringify(allData),
+		async: true,
+		type: "POST",
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		success: function(rt, jqXHR) {
+			console.log(rt);
+			var data = rt.resultData;
 
-						  $("#sggUl li").remove();
-						  $("#sgg option").remove();
+			$("#sggUl li").remove();
+			$("#sgg option").remove();
 
-						  $("#sggUl").append("<li><p>전체</p></li>");
-						  $("#sgg").append("<option value=''>전체</option>");
-						  for(var i=0;i<data.length;i++){
-							$("#sggUl").append("<li><p>"+data[i].sm_gugun+"</p></li>");
-							$("#sgg").append("<option>"+data[i].sm_gugun+"</option>");
-						  }
+			$("#sggUl").append("<li><p>전체</p></li>");
+			$("#sgg").append("<option value=''>전체</option>");
+			for (var i = 0; i < data.length; i++) {
+				$("#sggUl").append("<li><p>" + data[i].sm_gugun + "</p></li>");
+				$("#sgg").append("<option>" + data[i].sm_gugun + "</option>");
+			}
 
-						  $("#sido").val($("#sido").val()).attr("selected","selected");
-					     // downloadExcel(rt.results);
-					    },
-					    beforeSend: function() {
-					      //(이미지 보여주기 처리)
-					      //$('#load').show();
-					    },
-					    complete: function() {
-					      //(이미지 감추기 처리)
-					      //$('#load').hide();
+			$("#sido").val($("#sido").val()).attr("selected", "selected");
+			// downloadExcel(rt.results);
+		},
+		beforeSend: function() {
+			//(이미지 보여주기 처리)
+			//$('#load').show();
+		},
+		complete: function() {
+			//(이미지 감추기 처리)
+			//$('#load').hide();
 
 
-					    },
-					    error: function(jqXHR, textStatus, errorThrown,responseText) {
-					      //alert("ajax error \n" + textStatus + " : " + errorThrown);
+		},
+		error: function(jqXHR, textStatus, errorThrown, responseText) {
+			//alert("ajax error \n" + textStatus + " : " + errorThrown);
 
-					      console.log(jqXHR);
-					      console.log(jqXHR.readyState);
-					      console.log(jqXHR.responseText);
-					      console.log(jqXHR.responseJSON);
+			console.log(jqXHR);
+			console.log(jqXHR.readyState);
+			console.log(jqXHR.responseText);
+			console.log(jqXHR.responseJSON);
 
-					    }
-					  }) //end ajax
+		}
+	}) //end ajax
 })
 
 
 
-$(document).on("change","#sgg",function(){
+$(document).on("change", "#sgg", function() {
 
-	var allData={"gugunKey":ljsIsNull($("#sgg option:selected").val())?'':$("#sgg option:selected").val(),"sidoKey":$("#sidoText").text()}
+	var allData = { "gugunKey": ljsIsNull($("#sgg option:selected").val()) ? '' : $("#sgg option:selected").val(), "sidoKey": $("#sidoText").text() }
 
-					  $.ajax({
+	$.ajax({
 
-					    url: "/land/api/getDongMaster",
-					    data:JSON.stringify(allData),
-					    async: true,
-					    type:"POST",
-					    dataType: "json",
-					    contentType: 'application/json; charset=utf-8',
-					    success: function(rt,jqXHR) {
-						  var data=rt.resultData;
-						  $("#emdUl li").remove();
-						  $("#emd option").remove();
-						  $("#emdUl").append("<li><p>전체</p></li>");
-						  $("#emd").append("<option value=''>전체</option>");
-						  for(var i=0;i<data.length;i++){
-							$("#emdUl").append("<li><p>"+data[i].bm_dong+"</p></li>");
-							$("#emd").append("<option>"+data[i].bm_dong+"</option>");
-						  }
-
-
-						  $("#sido").val($("#sidoText").text()).attr("selected","selected");
-						  $("#sgg").val($("#sggText").text()).attr("selected","selected");
-					     // downloadExcel(rt.results);
-					    },
-					    beforeSend: function() {
-					      //(이미지 보여주기 처리)
-					      //$('#load').show();
-					    },
-					    complete: function() {
-					      //(이미지 감추기 처리)
-					      //$('#load').hide();
+		url: "/land/api/getDongMaster",
+		data: JSON.stringify(allData),
+		async: true,
+		type: "POST",
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		success: function(rt, jqXHR) {
+			var data = rt.resultData;
+			$("#emdUl li").remove();
+			$("#emd option").remove();
+			$("#emdUl").append("<li><p>전체</p></li>");
+			$("#emd").append("<option value=''>전체</option>");
+			for (var i = 0; i < data.length; i++) {
+				$("#emdUl").append("<li><p>" + data[i].bm_dong + "</p></li>");
+				$("#emd").append("<option>" + data[i].bm_dong + "</option>");
+			}
 
 
-					    },
-					    error: function(jqXHR, textStatus, errorThrown,responseText) {
-					      //alert("ajax error \n" + textStatus + " : " + errorThrown);
+			$("#sido").val($("#sidoText").text()).attr("selected", "selected");
+			$("#sgg").val($("#sggText").text()).attr("selected", "selected");
+			// downloadExcel(rt.results);
+		},
+		beforeSend: function() {
+			//(이미지 보여주기 처리)
+			//$('#load').show();
+		},
+		complete: function() {
+			//(이미지 감추기 처리)
+			//$('#load').hide();
 
-					      console.log(jqXHR);
-					      console.log(jqXHR.readyState);
-					      console.log(jqXHR.responseText);
-					      console.log(jqXHR.responseJSON);
 
-					    }
-					  }) //end ajax
+		},
+		error: function(jqXHR, textStatus, errorThrown, responseText) {
+			//alert("ajax error \n" + textStatus + " : " + errorThrown);
+
+			console.log(jqXHR);
+			console.log(jqXHR.readyState);
+			console.log(jqXHR.responseText);
+			console.log(jqXHR.responseJSON);
+
+		}
+	}) //end ajax
 })
 
 
-$(document).on("change","#emd",function(){
-	$("#sido").val($("#sidoText").text()).attr("selected","selected");
-	$("#sgg").val($("#sggText").text()).attr("selected","selected");
-	$("#emd").val($("#emdText").text()).attr("selected","selected");
-	var allData={"dongKey":$("#emdText").text(),"gugunKey":$("#sggText").text(),"sidoKey":$("#sidoText").text()}
+$(document).on("change", "#emd", function() {
+	$("#sido").val($("#sidoText").text()).attr("selected", "selected");
+	$("#sgg").val($("#sggText").text()).attr("selected", "selected");
+	$("#emd").val($("#emdText").text()).attr("selected", "selected");
+	var allData = { "dongKey": $("#emdText").text(), "gugunKey": $("#sggText").text(), "sidoKey": $("#sidoText").text() }
 
-					  $.ajax({
+	$.ajax({
 
-					    url: "/land/api/getRiMaster",
-					    data:JSON.stringify(allData),
-					    async: true,
-					    type:"POST",
-					    dataType: "json",
-					    contentType: 'application/json; charset=utf-8',
-					    success: function(rt,jqXHR) {
+		url: "/land/api/getRiMaster",
+		data: JSON.stringify(allData),
+		async: true,
+		type: "POST",
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		success: function(rt, jqXHR) {
 
-						  var data=rt.resultData;
-						  $("#riUl li").remove();
-						  $("#ri option").remove();
-						  $("#riUl").append("<li><p>전체</p></li>");
-						  $("#ri").append("<option value=''>전체</option>");
-						  for(var i=0;i<data.length;i++){
-							$("#riUl").append("<li><p>"+data[i].rm_ri+"</p></li>");
-							$("#ri").append("<option>"+data[i].rm_ri+"</option>");
-						  }
-
-
-						 /* $("#sido").val($("#sidoText").text()).attr("selected","selected");
-						  $("#sgg").val($("#sggText").text()).attr("selected","selected");*/
-					     // downloadExcel(rt.results);
-					    },
-					    beforeSend: function() {
-					      //(이미지 보여주기 처리)
-					      //$('#load').show();
-					    },
-					    complete: function() {
-					      //(이미지 감추기 처리)
-					      //$('#load').hide();
+			var data = rt.resultData;
+			$("#riUl li").remove();
+			$("#ri option").remove();
+			$("#riUl").append("<li><p>전체</p></li>");
+			$("#ri").append("<option value=''>전체</option>");
+			for (var i = 0; i < data.length; i++) {
+				$("#riUl").append("<li><p>" + data[i].rm_ri + "</p></li>");
+				$("#ri").append("<option>" + data[i].rm_ri + "</option>");
+			}
 
 
-					    },
-					    error: function(jqXHR, textStatus, errorThrown,responseText) {
-					      //alert("ajax error \n" + textStatus + " : " + errorThrown);
+			/* $("#sido").val($("#sidoText").text()).attr("selected","selected");
+			 $("#sgg").val($("#sggText").text()).attr("selected","selected");*/
+			// downloadExcel(rt.results);
+		},
+		beforeSend: function() {
+			//(이미지 보여주기 처리)
+			//$('#load').show();
+		},
+		complete: function() {
+			//(이미지 감추기 처리)
+			//$('#load').hide();
 
-					      console.log(jqXHR);
-					      console.log(jqXHR.readyState);
-					      console.log(jqXHR.responseText);
-					      console.log(jqXHR.responseJSON);
 
-					    }
-					  }) //end ajax
+		},
+		error: function(jqXHR, textStatus, errorThrown, responseText) {
+			//alert("ajax error \n" + textStatus + " : " + errorThrown);
+
+			console.log(jqXHR);
+			console.log(jqXHR.readyState);
+			console.log(jqXHR.responseText);
+			console.log(jqXHR.responseJSON);
+
+		}
+	}) //end ajax
 })
 
 
 
 
-function datatablebasic(){
+function datatablebasic() {
 	new DataTable('#basicTable', {
-		 fixedColumns:{
-			start:3
-		 },
-    paging: true,
-    scrollCollapse: true,
-    scrollX: true,
-    scrollY: 300,
-    dom:'rtip',
-    columnDefs:[
-					{"className": "dt-head-center", "targets": "_all"},
-					{className: 'dt-center',"targets": "_all"},
-					{targets:[0],width:"100px"},
-					{targets:[1],width:"300px"},
-				]
+		fixedColumns: {
+			start: 3
+		},
+		paging: true,
+		scrollCollapse: true,
+		scrollX: true,
+		scrollY: 300,
+		dom: 'rtip',
+		columnDefs: [
+			{ "className": "dt-head-center", "targets": "_all" },
+			{ className: 'dt-center', "targets": "_all" },
+			{ targets: [0], width: "100px" },
+			{ targets: [1], width: "300px" },
+		]
 	});
 }
 
@@ -303,11 +303,6 @@ function loadDataTable(params) {
 				//$("div.dt-title").html('<div class="dataTitles"><h5>총 검색 건 수</h5></div>');
 				return json.data;
 			}
-
-
-
-
-
 		},
 		initComplete: function() {
 
@@ -441,10 +436,10 @@ return nRow;
 	// console.log($('#userTable').DataTable().page.info().recordsTotal);
 }
 
-$(document).on("click","#moveMap",function(){
+$(document).on("click", "#moveMap", function() {
 	//openMapWindow();
-//	mapWindow = window.open('', 'mapWindow', 'width=2000,height=1000');
+	//	mapWindow = window.open('', 'mapWindow', 'width=2000,height=1000');
 	const x = $(this).attr('x')
 	const y = $(this).attr('y')
-	moveToCityHall(x,y);
+	moveToCityHall(x, y);
 })
