a5f36e3a975ff6f54f46aec906a18b6ad9619580
jschoi <jschoi@slsolution.co.kr>
2024-10-07 18:50:56 +0900

    지상권 해지 상세 페이지 다운로드, 보기 버튼 기능 개선

--- plms2/src/main/resources/static/js/jisang/landTerminationRegistration1.js
+++ plms2/src/main/resources/static/js/jisang/landTerminationRegistration1.js
@@ -776,19 +776,10 @@ $(document).on("click","#docFileDelBtn",function(){
 })
 
 //보기 클릭 시 팝업 기능
-function openFilePopup(filePath) {
-    // 절대 경로를 사용하도록 file:// 스킴을 추가
-    const serverUrl = `/api/downloadFile?filePath=` + encodeURIComponent(filePath);
-
-    // 새 창의 옵션 설정 (예: 너비 600px, 높이 400px, 스크롤바 허용 등)
-    const popupOptions = "width=800,height=600,scrollbars=yes,resizable=yes";
-    // 새 창 열기
-    window.open(serverUrl, '파일 보기', popupOptions);
+function openFilePopup(filePath, fileName, fileJisangNo, fileSeq, fileGubun) {
+    commonFileView(filePath, fileName, fileJisangNo, fileSeq, fileGubun);
 }
 
-function downloadFile(filePath, fileName) {
-      const url = `/api/download?filePath=${filePath}&fileName=${encodeURIComponent(fileName)}`;
-	 //  const url = `/api/download?filePath=${filePath}&fileName=${fileName}`;
-	   console.log(url);
-       window.open(url, '_blank');  // 새 창이나 새 탭에서 파일 다운로드
-   }
+function downloadFile(filePath, fileName, fileJisangNo, fileSeq, fileGubun) {
+	commonFileDownload(filePath, fileName, fileJisangNo, fileSeq, fileGubun);
+}
